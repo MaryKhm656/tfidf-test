6eeca078c2a3f5a177eebf04367be9b050de0d0c
MaryKhm656 <nanoman66656@gmail.com>
2025-04-16 09:54:15 +0300

    Added idf counter

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
-import re
 from collections import Counter
+import math
 
 app = Flask(__name__)
 
@@ -8,19 +8,30 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-def proccess_text(text):
+def process_text(text):
     text = text.lower()
-    words = re.findall(r'\b\w+\b', text)
+    for ch in ".,?!;:()[]«»\'\"":
+        text = text.replace(ch, " ")
+    words = text.split()
     tf_counter = Counter(words)
     most_common_words = tf_counter.most_common(50)
-    return most_common_words
+    result = []
+    for word, tf in most_common_words:
+        idf = math.log(1 + 1/tf)
+        result.append({
+            "word": word,
+            "tf": tf,
+            "idf": round(idf, 6)
+        })
+    result.sort(key=lambda x: x["idf"], reverse=True)
+    return result
 
 @app.route("/upload", methods = ["POST"])
 def upload_file():
     uploaded_file = request.files["file"]
     if uploaded_file.filename.endswith(".txt"):
         content = uploaded_file.read().decode("utf-8")
-        words = proccess_text(content)
+        words = process_text(content)
         return str(words)
     else:
         return "Пожалуйста, загрузите .txt файл"
